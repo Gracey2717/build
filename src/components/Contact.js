9a479798c95a57847953349ee23d7cbf2ef01358
Owolabi Grace <owolabigracemercy@gmail.com>
2025-11-22 09:48:18 +0300

    My local work commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -55,7 +55,7 @@ function Contact() {
         <div className="map">
           <iframe
             title="company-location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63403.97329685211!2d3.443411221679691!3d6.6780700000000195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103be942e730985d%3A0x83d3a38ae7dfeba1!2sBestcrew%20Property%20Ltd!5e0!3m2!1sen!2sng!4v1763791217434!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63403.97329685211!2d3.443411221679691!3d6.6780700000000195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103be942e730985d%3A0x83d3a38ae7dfeba1!2sBestcrew%20Property%20Ltd!5e0!3m2!1sen!2sng!4v1763791217434!5m2!1sen!2sng" 
             width="100%"
             height="300"
             style={{ border: 0 }}
